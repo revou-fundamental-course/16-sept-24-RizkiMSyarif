efe5e76edb246dddfd5b913bcd7e6e03e70e2803
Rizki Muhammad Syarif <rizkimsyarif100402@gmail.com>
2024-09-24 22:42:36 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-//ini js
+function replaceNama() {
+    let nama = prompt("Siapakah nama anda?", "");
+    document.getElementById("nama").innerHTML = nama
+}
+
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceNama();
+})
