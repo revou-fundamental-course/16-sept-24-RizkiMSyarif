7e12154d421b1094da9859b92925563c12bbe086
Rizki Muhammad Syarif <rizkimsyarif100402@gmail.com>
2024-09-24 23:30:51 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-function replaceNama() {
-    let nama = prompt("Siapakah nama anda?", "");
-    document.getElementById("nama").innerHTML = nama
-}
+//ini java
+let nameInput = document.getElementById('name-input')
 
-document.getElementById('tombol').addEventListener("click", function() {
-    replaceNama();
+document.getElementById('kirim').addEventListener("click", function() {
+    const nami = nameInput.value
+
+    if (nama == "") {
+        document.getElementById("error-name").innerHTML = "Nama tidak boleh kosong"
+    } else {
+        document.getElementById('nama').innerHTML = nami
+    }
+
 })
